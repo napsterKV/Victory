b4fdf95d4223e706234eae1ae248643fcc32d0e5
aleksandraivanets <aleksandraivanets@gmail.com>
2021-05-25 21:20:37 +0300

    playlists-ivanets

--- scripts/browse.js
+++ scripts/browse.js
@@ -21,12 +21,12 @@ function newReleasesLoaded(data) {
   const releases_arr = [];
   for (const releas_item of data.albums.items) {
     const img = document.createElement("img");
-    img.className = 'release_img';
+    img.className = "release_img";
     img.src = releas_item.images[1].url;
     const name = document.createElement("h4");
     name.textContent = releas_item.name;
     const container = document.createElement("div");
-    container.className = 'release';
+    container.className = "release";
     container.appendChild(img);
     container.appendChild(name);
     releases_arr.push(container);
@@ -35,7 +35,6 @@ function newReleasesLoaded(data) {
 }
 
 function playlistsLoaded(data) {
-
   const playlistsElem = document.querySelector("#featuredPlaylists");
 
   const playlists_arr = [];
@@ -43,11 +42,21 @@ function playlistsLoaded(data) {
   for (const playlist_item of data.playlists.items) {
     const img = document.createElement("img");
     img.src = playlist_item.images[0].url;
+    img.className = "playlist-style";
     const name = document.createElement("h4");
+    name.className = "playlist-text";
     name.textContent = playlist_item.name;
     const container = document.createElement("div");
+    container.className = "playlist-container";
+    const description = document.createElement("h3");
+    description.className = "playlist-description";
+    description.textContent = playlist_item.description;
+    const textContainer = document.createElement("div");
     container.appendChild(img);
-    container.appendChild(name);
+    textContainer.appendChild(name);
+
+    textContainer.appendChild(description);
+    container.appendChild(textContainer);
     playlists_arr.push(container);
   }
   playlistsElem.append(...playlists_arr);
