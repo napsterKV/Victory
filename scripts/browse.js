b4c6b743e4dcfd0da9b39133bde2306b7de9c6c2
Napster-YT <a.abdusaliamov@gmail.com>
2021-05-31 19:03:14 +0300

    dameged css & html

--- scripts/browse.js
+++ scripts/browse.js
@@ -71,36 +71,4 @@ function playlistsLoaded(data) {
   playlistsElem.append(...playlists_arr);
 }
 
-// const btn = document.querySelector("#navToggler");
-// const navMenu = document.querySelector("nav__menu");
-// const dimmer = document.querySelector("dimmer");
 
-// btn.addEventListener("click", () => {
-//   navMenu.classList.toggler("nav__menu_open");
-//   dimmer.classList.toggler("dimmer_active");
-// });
-
-// dimmer.addEventListener("click", () => {
-//   navMenu.classList.remove("nav__menu_open");
-//   dimmer.classList.remove("dimmer_active");
-// });
-
-const btn = document.querySelector("#navToggler");
-const navMenu = document.querySelector(".nav__menu");
-const dimmer = document.querySelector(".dimmer");
-
-btn.addEventListener("click", () => {
-  navMenu.classList.toggle("nav__menu_open");
-  dimmer.classList.toggle("dimmer_active");
-});
-
-if (navMenu.classList.contains("nav__menu_open")) {
-  dimmer.classList.add("dimmer_active");
-} else {
-  dimmer.classList.remove("dimmer_active");
-}
-
-dimmer.addEventListener("click", () => {
-  navMenu.classList.remove("nav__menu_open");
-  dimmer.classList.remove("dimmer_active");
-});
--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,26 @@
 //   navMenu.classList.remove("nav__menu_open");
 //   dimmer.classList.remove("dimmer_active");
 // });
+
+/////////////////
+
+////////////////
+const btn = document.querySelector("#navToggler");
+const navMenu = document.querySelector(".nav__menu");
+const dimmer = document.querySelector(".dimmer");
+
+btn.addEventListener("click", () => {
+  navMenu.classList.toggle("nav__menu_open");
+  dimmer.classList.toggle("dimmer_active");
+});
+
+if (navMenu.classList.contains("nav__menu_open")) {
+  dimmer.classList.add("dimmer_active");
+} else {
+  dimmer.classList.remove("dimmer_active");
+}
+
+dimmer.addEventListener("click", () => {
+  navMenu.classList.remove("nav__menu_open");
+  dimmer.classList.remove("dimmer_active");
+});
